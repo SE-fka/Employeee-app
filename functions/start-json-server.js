6efeecf3723f0b104fb796f719f3f689ba63bb4c
Fka <test@gmail.com>
2024-07-11 16:45:32 +0300

    first commit

--- functions/start-json-server.js
+++ functions/start-json-server.js
@@ -1,27 +1,19 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
 exports.handler = async (event, context) => {
-    const { spawn } = require('child_process');
-  
-    return new Promise((resolve, reject) => {
-      const jsonServer = spawn('json-server', ['--watch', 'db.json', '--port', '8080']);
-  
-      jsonServer.stdout.on('data', (data) => {
-        console.log(`stdout: ${data}`);
-      });
-  
-      jsonServer.stderr.on('data', (data) => {
-        console.error(`stderr: ${data}`);
-      });
-  
-      jsonServer.on('error', (error) => {
-        reject(error);
-      });
-  
-      jsonServer.on('close', (code) => {
-        console.log(`child process exited with code ${code}`);
-        resolve({
-          statusCode: 200,
-          body: 'JSON server started successfully',
-        });
+  server.use(middlewares);
+  server.use(router);
+  return new Promise((resolve, reject) => {
+    const port = 3000;
+    server.listen(port, () => {
+      console.log(`JSON Server is running on port ${port}`);
+      resolve({
+        statusCode: 200,
+        body: JSON.stringify({ message: 'JSON Server started' }),
       });
     });
-  };
+  });
+};
